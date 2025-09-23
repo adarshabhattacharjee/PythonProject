caa34fc9271448ef38b8290b178d0cb9dbf6e200
adarsha <bhattacharjeeadarsha@gmail.com>
2025-09-23 23:57:01 +0530

    Initial Commit

---
+++ AdarshaBhattacharjee_RUGVED/Task _1/Prob_15.py
@@ -0,0 +1,7 @@
+def rotate90(mat):
+    n = len(mat)
+    for i in range(n):
+        for j in range(i + 1, n):
+            mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+    for row in mat:
+        row.reverse()
