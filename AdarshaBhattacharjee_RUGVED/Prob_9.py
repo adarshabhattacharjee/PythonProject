052d36b1eb0d45123b548d28873b07e4c12a1653
adarsha <bhattacharjeeadarsha@gmail.com>
2025-09-23 17:43:48 +0530

    Initial Commit

---
+++ AdarshaBhattacharjee_RUGVED/Prob_9.py
@@ -0,0 +1,11 @@
+
+def caesar_cipher_encrypt(text, shift):
+    result = ''
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            encrypted_char = chr((ord(char) - base + shift) % 26 + base)
+            result += encrypted_char
+        else:
+            result += char
+    return result
