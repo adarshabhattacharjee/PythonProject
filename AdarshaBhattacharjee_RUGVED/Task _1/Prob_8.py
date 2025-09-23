9f4078335a5cb2cc4aa44b533f0043fd9f18e4a7
adarsha <bhattacharjeeadarsha@gmail.com>
2025-09-23 17:14:31 +0530

    Commit this change

--- AdarshaBhattacharjee_RUGVED/Task _1/Prob_8.py
+++ AdarshaBhattacharjee_RUGVED/Task _1/Prob_8.py
@@ -1,7 +1,8 @@
-string=input("Enter a string: ")
-sort=''.join(sorted(string))
-current_char = sort[0]
-count = 0
+def divide_string(string, n):
+    if len(string) % n != 0:
+        return "Error: String length not divisible by n."
+    num_parts = len(string) // n
+    parts = [string[i * n: (i + 1) * n] for i in range(num_parts)]
 
 for char in sort:
     if char == current_char:
