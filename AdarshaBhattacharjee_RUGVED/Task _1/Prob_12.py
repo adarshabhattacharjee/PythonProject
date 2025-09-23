7e5607a4e9a9878776900bc300220dec57732dbf
adarsha <bhattacharjeeadarsha@gmail.com>
2025-09-23 23:19:47 +0530

    Initial Commit

--- AdarshaBhattacharjee_RUGVED/Task _1/Prob_12.py
+++ AdarshaBhattacharjee_RUGVED/Task _1/Prob_12.py
@@ -1,4 +1,9 @@
-inp=input("enter the number")
-print("Input : ",inp)
+n=input("enter the number")
+print("Input : ",n)
+print('Output:')
 i=0
+for i in range(1, int(n)+1):
+    print(" " * (int(n) - i) + "* " * i)
+for i in range(int(n) - 1, 0, -1):
+    print(" " * (int(n) - i) + "* " * i)
 
